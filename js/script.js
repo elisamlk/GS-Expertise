744d252c7fbaa39b8997bf897acf2d5d7a701845
Elisabeth Malek <elisabeth.malek@yahoo.fr>
2023-03-06 18:37:12 +0100

    Changement couleur photo au hover

--- js/script.js
+++ js/script.js
@@ -1,13 +1,25 @@
 $(".handle").on("click", function () {
-    console.log("ok");
-    $("header").toggleClass("mobile-device");
-    if ($("header").hasClass("mobile-device")) {
-      $("nav").addClass("showing");
-      $("i").removeClass("fa-solid fa-bars");
-      $("i").addClass("fa-solid fa-xmark");
-    } else {
-      $("i").removeClass("fa-solid fa-xmark");
-      $("i").addClass("fa-solid fa-bars");
-      $("nav").removeClass("showing");
-    }
-  });
+  console.log("ok");
+  $("header").toggleClass("mobile-device");
+  if ($("header").hasClass("mobile-device")) {
+    $("nav").addClass("showing");
+    $("i").removeClass("fa-solid fa-bars");
+    $("i").addClass("fa-solid fa-xmark");
+  } else {
+    $("i").removeClass("fa-solid fa-xmark");
+    $("i").addClass("fa-solid fa-bars");
+    $("nav").removeClass("showing");
+  }
+});
+
+$(document).ready(function(){  
+  $(".img-bl").hover(function(){  
+    console.log("ok")
+    $(this).attr('src','img/accueil.png');  
+    }, function(){  
+    $(this).attr('src','img/photo-accueil-header-equipe-cabinet-gsexpertise.png');  
+  });  
+}); 
+
+
+
